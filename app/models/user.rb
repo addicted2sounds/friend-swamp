c73699ab9313d85e8730df5aa71f78c86d2d5c56
addicted <addicted22sounds@gmail.com>
2015-06-04 17:53:57 +0300

    added friends and inverse friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :friendship_requests
+  has_many :inverse_friendship_request, class_name: 'FriendshipRequest', foreign_key: :friend_id
+
+  has_many :friends, through: :friendship_requests
+  has_many :inverse_friends, through: :inverse_friendship_request, source: :user
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'friends' do
+    let(:request) { create :friendship_request, status: :accepted }
+    it 'has friend through request' do
+      expect(request.user.friends).to include request.friend
+    end
+
+    it 'has friend through request (inverse)' do
+      expect(request.friend.inverse_friends).to include request.user
+    end
+  end
 end
