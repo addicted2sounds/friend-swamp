af70e906d268d3097653c14f75a72be7585973f7
addicted <addicted22sounds@gmail.com>
2015-06-04 16:11:44 +0300

    view tests

--- spec/views/friendship_requests/index.html.slim_spec.rb
+++ spec/views/friendship_requests/index.html.slim_spec.rb
@@ -1,22 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "friendship_requests/index", type: :view do
+RSpec.describe 'friendship_requests/index', type: :view do
+  let(:friendship_request) { create :friendship_request}
   before(:each) do
     assign(:friendship_requests, [
-      FriendshipRequest.create!(
-        :user => nil,
-        :friend => nil
-      ),
-      FriendshipRequest.create!(
-        :user => nil,
-        :friend => nil
-      )
+       friendship_request
     ])
+    render
   end
 
-  it "renders a list of friendship_requests" do
-    render
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+  it 'includes user email in table' do
+    expect(rendered).to have_selector 'tr>td', :text => friendship_request.user.email, :count => 1
+  end
+
+  it 'has show link' do
+
   end
 end
