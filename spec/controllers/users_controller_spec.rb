e5dd321d183139187dd71405750059818be898f6
addicted <addicted22sounds@gmail.com>
2015-06-04 14:18:52 +0300

    added devise to controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -20,7 +20,9 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   let(:user) { create :user }
-
+  before :each do
+    sign_in user
+  end
   describe "GET #index" do
     it "assigns all users as @users" do
       get :index
