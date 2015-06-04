7199208cc302bf1bbe42ecc1a233c915518c3352
addicted <addicted22sounds@gmail.com>
2015-06-04 14:07:15 +0300

    added users index spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -19,30 +19,14 @@ require 'rails_helper'
 # that an instance is receiving a specific message.
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) { create :user }
 
-  # This should return the minimal set of attributes required to create a valid
-  # User. As you add validations to User, be sure to
-  # adjust the attributes here as well.
-  # let(:valid_attributes) {
-  #   skip("Add a hash of attributes valid for your model")
-  # }
-  #
-  # let(:invalid_attributes) {
-  #   skip("Add a hash of attributes invalid for your model")
-  # }
-  #
-  # # This should return the minimal set of values that should be in the session
-  # # in order to pass any filters (e.g. authentication) defined in
-  # # UsersController. Be sure to keep this updated too.
-  # let(:valid_session) { {} }
-  #
-  # describe "GET #index" do
-  #   it "assigns all users as @users" do
-  #     user = User.create! valid_attributes
-  #     get :index, {}, valid_session
-  #     expect(assigns(:users)).to eq([user])
-  #   end
-  # end
+  describe "GET #index" do
+    it "assigns all users as @users" do
+      get :index
+      expect(assigns(:users)).to eq([user])
+    end
+  end
   #
   # describe "GET #show" do
   #   it "assigns the requested user as @user" do
