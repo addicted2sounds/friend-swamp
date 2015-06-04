239b5993eb931c2c20c5c6b5c18a4ea4499921c5
addicted <addicted22sounds@gmail.com>
2015-06-04 16:32:16 +0300

    added status enum to request

--- app/models/friendship_request.rb
+++ app/models/friendship_request.rb
@@ -1,4 +1,5 @@
 class FriendshipRequest < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+  enum status: [:pending, :accepted, :declined]
 end
--- spec/factories/friendship_requests.rb
+++ spec/factories/friendship_requests.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :friendship_request do
     user
     association :friend, factory: :user
+    status :pending
   end
 end
